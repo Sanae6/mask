a73c94e7d4fbaffaffe1db900881e0c00db393d8
Aubrey Taylor <aubrey@hall.ly>
2026-01-31 11:49:48 -0600

    export parameters

--- Player.cs
+++ Player.cs
@@ -1,61 +1,64 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
 public partial class Player : CharacterBody2D {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() {
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+    }
 
-	private int coyote;
+    private int coyote;
     private float jumpGrav;
-    private const int CoyoteDuration = 16;
-    private const float JumpBurst = -1000;
-    private const float JumpGravMultiplier = 50;
-    private const float JumpGravReleaseMin = 100;
-    private const float JumpHoldDuration = 20;
-    private const float SpeedCap = 800;
-    private const int GroundFriction = 80;
-    private const int AirFriction = 80;
-    private const int MoveMultiplier = 80;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta) {
-		Vector2 newVel = Velocity;
-		if (!IsOnFloor()) {
-			coyote = Mathf.Max(coyote - 1, 0);
-            jumpGrav += JumpGravMultiplier * (float) delta;
+    [Export] private int coyoteDuration = 16;
+    [Export] private float jumpBurst = -1000;
+    [Export] private float jumpGravMultiplier = 50;
+    [Export] private float jumpGravReleaseMin = 100;
+    [Export] private float jumpHoldDuration = 10;
+    [Export] private float speedCap = 800;
+    [Export] private int turnAroundDelta = 200;
+    [Export] private int groundFriction = 80;
+    [Export] private int airFriction = 80;
+    [Export] private int moveMultiplier = 80;
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(double delta) {
+        Vector2 newVel = Velocity;
+        if (!IsOnFloor()) {
+            coyote = Mathf.Max(coyote - 1, 0);
+            jumpGrav += jumpGravMultiplier * (float)delta;
             newVel.Y += jumpGrav;
         } else {
-			coyote = CoyoteDuration;
+            coyote = coyoteDuration;
+            jumpGrav = jumpGravReleaseMin;
+        }
+
+        if (Input.IsActionJustPressed("ui_accept") && coyote > 0) {
+            newVel.Y = -jumpBurst;
+            coyote = 0;
             jumpGrav = 0;
         }
 
-		if (Input.IsActionJustPressed("ui_accept") && coyote > 0) {
-			newVel.Y = -JumpBurst;
-			coyote = 0;
-            jumpGrav = 0;
-        }
-
-        if (jumpGrav < JumpHoldDuration && Input.IsActionPressed("ui_accept")) {
-            newVel.Y = JumpBurst;
+        if (jumpGrav < jumpHoldDuration && Input.IsActionPressed("ui_accept")) {
+            newVel.Y = jumpBurst;
         } else {
-            jumpGrav = Mathf.Max(jumpGrav, JumpGravReleaseMin);
+            jumpGrav = Mathf.Max(jumpGrav, jumpGravReleaseMin);
         }
 
-        
-		float axis = Input.GetAxis("ui_left", "ui_right");
-		int axisSign = Mathf.Sign(axis);
-		if (axis != 0 && (newVel.X == 0 || axisSign == Mathf.Sign(newVel.X))) {
-			newVel.X += axis * MoveMultiplier;
-		} else {
-			newVel.X = Mathf.MoveToward(newVel.X, 0, IsOnFloor() ? GroundFriction : AirFriction);
-		}
 
-        newVel.X = Mathf.Clamp(newVel.X, -SpeedCap, SpeedCap);
+        float axis = Input.GetAxis("ui_left", "ui_right");
+        if (axis != 0 && (newVel.X == 0 || Mathf.Sign(axis) == Mathf.Sign(newVel.X))) {
+            newVel.X += axis * moveMultiplier;
+        } else {
+            newVel.X = -Mathf.Sign(axis) == Mathf.Sign(newVel.X)
+                ? Mathf.MoveToward(newVel.X, 0, 200)
+                : Mathf.MoveToward(newVel.X, 0, IsOnFloor() ? groundFriction : airFriction);
+        }
 
-		Velocity = newVel;
+        newVel.X = Mathf.Clamp(newVel.X, -speedCap, speedCap);
 
-		MoveAndSlide();
-	}
+        Velocity = newVel;
 
+        MoveAndSlide();
+    }
 }
