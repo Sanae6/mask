16350f8956ebfabbccc6f7ab56a4f6527810214d
Aubrey Taylor <aubrey@hall.ly>
2026-01-31 00:01:15 -0600

    parameterize player

--- Player.cs
+++ Player.cs
@@ -6,50 +6,56 @@ public partial class Player : CharacterBody2D {
 	public override void _Ready() {
 	}
 
-	private int coyote = 0;
-    private float jumpTime = 0;
-    private float jumpGrav = 0;
-    private const float JumpDuration = 20;
+	private int coyote;
+    private float jumpGrav;
+    private const int CoyoteDuration = 16;
+    private const float JumpBurst = -1000;
+    private const float JumpGravMultiplier = 50;
+    private const float JumpGravReleaseMin = 100;
+    private const float JumpHoldDuration = 20;
+    private const float SpeedCap = 800;
+    private const int GroundFriction = 80;
+    private const int AirFriction = 80;
+    private const int MoveMultiplier = 80;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta) {
-		var newVel = Velocity;
+		Vector2 newVel = Velocity;
 		if (!IsOnFloor()) {
 			coyote = Mathf.Max(coyote - 1, 0);
-            jumpGrav += 50 * (float) delta;
+            jumpGrav += JumpGravMultiplier * (float) delta;
             newVel.Y += jumpGrav;
         } else {
-			coyote = 16;
+			coyote = CoyoteDuration;
             jumpGrav = 0;
         }
 
 		if (Input.IsActionJustPressed("ui_accept") && coyote > 0) {
-			newVel.Y = -1000;
+			newVel.Y = -JumpBurst;
 			coyote = 0;
-            jumpTime = JumpDuration;
             jumpGrav = 0;
         }
 
-        if (jumpGrav < 10 && Input.IsActionPressed("ui_accept")) {
-            newVel.Y = -1000;
+        if (jumpGrav < JumpHoldDuration && Input.IsActionPressed("ui_accept")) {
+            newVel.Y = JumpBurst;
         } else {
-            jumpGrav = Mathf.Max(jumpGrav, 100);
+            jumpGrav = Mathf.Max(jumpGrav, JumpGravReleaseMin);
         }
 
         
-		var axis = Input.GetAxis("ui_left", "ui_right");
-		var axisSign = Mathf.Sign(axis);
+		float axis = Input.GetAxis("ui_left", "ui_right");
+		int axisSign = Mathf.Sign(axis);
 		if (axis != 0 && (newVel.X == 0 || axisSign == Mathf.Sign(newVel.X))) {
-			newVel.X += axis * 30;
+			newVel.X += axis * MoveMultiplier;
 		} else {
-			newVel.X = Mathf.MoveToward(newVel.X, 0, IsOnFloor() ? 100 : 80);
+			newVel.X = Mathf.MoveToward(newVel.X, 0, IsOnFloor() ? GroundFriction : AirFriction);
 		}
 
-		const float speedCap = 800;
-		newVel.X = Mathf.Clamp(newVel.X, -speedCap, speedCap);
+        newVel.X = Mathf.Clamp(newVel.X, -SpeedCap, SpeedCap);
 
 		Velocity = newVel;
 
 		MoveAndSlide();
 	}
+
 }
