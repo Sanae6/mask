a0265511a23ce6dbf83776a1697536aba7275eb5
Aubrey Taylor <aubrey@hall.ly>
2026-01-30 23:36:22 -0600

    moving player

--- Player.cs
+++ Player.cs
@@ -7,33 +7,45 @@ public partial class Player : CharacterBody2D {
 	}
 
 	private int coyote = 0;
-    private int jumpTime = 0; 
+    private float jumpTime = 0;
+    private float jumpGrav = 0;
+    private const float JumpDuration = 20;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta) {
 		var newVel = Velocity;
 		if (!IsOnFloor()) {
-            if (newVel.Y)
-			newVel.Y -= -1000 * (float)delta;
 			coyote = Mathf.Max(coyote - 1, 0);
-		} else {
+            jumpGrav += 50 * (float) delta;
+            newVel.Y += jumpGrav;
+        } else {
 			coyote = 16;
-		}
+            jumpGrav = 0;
+        }
 
 		if (Input.IsActionJustPressed("ui_accept") && coyote > 0) {
-			newVel.Y = -400;
+			newVel.Y = -1000;
 			coyote = 0;
-		}
+            jumpTime = JumpDuration;
+            jumpGrav = 0;
+        }
 
+        if (jumpGrav < 10 && Input.IsActionPressed("ui_accept")) {
+            newVel.Y = -1000;
+        } else {
+            jumpGrav = Mathf.Max(jumpGrav, 100);
+        }
+
+        
 		var axis = Input.GetAxis("ui_left", "ui_right");
 		var axisSign = Mathf.Sign(axis);
 		if (axis != 0 && (newVel.X == 0 || axisSign == Mathf.Sign(newVel.X))) {
 			newVel.X += axis * 30;
 		} else {
-			newVel.X = Mathf.MoveToward(newVel.X, 0, IsOnFloor() ? 80 : 60);
+			newVel.X = Mathf.MoveToward(newVel.X, 0, IsOnFloor() ? 100 : 80);
 		}
 
-		const float speedCap = 500;
+		const float speedCap = 800;
 		newVel.X = Mathf.Clamp(newVel.X, -speedCap, speedCap);
 
 		Velocity = newVel;
